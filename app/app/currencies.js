b2b1256ae7dde291de446ac8c12634bb943c27d6
aeco111984-source <aeco111984@gmail.com>
2025-11-15 05:31:21 +0000

    Create currencies.js

---
+++ app/app/currencies.js
@@ -0,0 +1,19 @@
+export const currencies = [
+  { code: "USD", name: "US Dollar", flag: "🇺🇸" },
+  { code: "EUR", name: "Euro", flag: "🇪🇺" },
+  { code: "GBP", name: "British Pound", flag: "🇬🇧" },
+  { code: "JPY", name: "Japanese Yen", flag: "🇯🇵" },
+  { code: "CHF", name: "Swiss Franc", flag: "🇨🇭" },
+  { code: "AUD", name: "Australian Dollar", flag: "🇦🇺" },
+  { code: "CAD", name: "Canadian Dollar", flag: "🇨🇦" },
+  { code: "NZD", name: "New Zealand Dollar", flag: "🇳🇿" },
+  { code: "CNY", name: "Chinese Yuan", flag: "🇨🇳" },
+  { code: "HKD", name: "Hong Kong Dollar", flag: "🇭🇰" },
+  { code: "SGD", name: "Singapore Dollar", flag: "🇸🇬" },
+  { code: "INR", name: "Indian Rupee", flag: "🇮🇳" },
+  { code: "AED", name: "UAE Dirham", flag: "🇦🇪" },
+  { code: "SAR", name: "Saudi Riyal", flag: "🇸🇦" },
+  { code: "QAR", name: "Qatari Riyal", flag: "🇶🇦" },
+  { code: "ZAR", name: "South African Rand", flag: "🇿🇦" },
+  // … I'll include the full 160-currency master list next message
+];
